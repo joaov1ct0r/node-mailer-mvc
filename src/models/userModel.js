142df9fe2680285a8340be1413dca6d8d9c7e3af
Joao Victor <jov9051@outlook.com>
2022-02-23 16:58:03 -0300

    userSchema definido em userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,1 +1,6 @@
 let mongoose = require('mongoose');
+
+let userSchema = mongoose.Schema({
+    nome: { type: String, required: true, minlength: 2, maxlength: 100 },
+    email: { type: String, required: true, minlength: 9, maxlength: 100 }
+});
