c5d8361ac6c439535c262913394a93646cc5dabd
Joao Victor <jov9051@outlook.com>
2022-02-23 17:43:36 -0300

    var checkUser checando se usuario ja e cadastrado

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,11 @@
 import User from '../models/userModel';
 
-let user = {};
+let user = {
+    async register(req, res) {
+        let checkUser = await User.findOne({ email: req.body.email });
+
+        if (checkUser) {
+            return res.status(400).send('Usuario ja cadastrado');
+        }
+    }
+};
