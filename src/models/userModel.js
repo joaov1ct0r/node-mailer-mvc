b08b0c24501d74310c2c4a02fee7884244ff467a
Joao Victor <jov9051@outlook.com>
2022-02-23 16:59:34 -0300

    model da schema sendo exportado em userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,3 +4,5 @@ let userSchema = mongoose.Schema({
     nome: { type: String, required: true, minlength: 2, maxlength: 100 },
     email: { type: String, required: true, minlength: 9, maxlength: 100 }
 });
+
+module.exports = mongoose.model('users', userSchema);
