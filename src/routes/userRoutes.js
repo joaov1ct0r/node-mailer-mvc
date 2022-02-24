e40fdea91e50b3d4b8a938728d985b90b2066fc7
Joao Victor <jov9051@outlook.com>
2022-02-24 18:55:58 -0300

    useEmail importado para userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 import userController from '../controllers/userController.js';
 
+import userEmail from '../controllers/userEmail.js';
+
 let router = express.Router();
 
 router.post('/cadastro', userController.register);
