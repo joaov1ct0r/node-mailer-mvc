88821262dfaada5e12523e4104b5ccd05e5bbb7e
Joao Victor <jov9051@outlook.com>
2022-02-24 19:12:27 -0300

    codigo inutilizado removido

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,8 +2,6 @@ import express from 'express';
 
 import userController from '../controllers/userController.js';
 
-import userEmail from '../controllers/userEmail.js';
-
 let router = express.Router();
 
 router.post('/cadastro', userController.register);
