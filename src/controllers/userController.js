666f020e840b11ffad8945b037978f47164cf1cd
Joao Victor <jov9051@outlook.com>
2022-03-27 18:22:38 -0300

    metodos em userController.js alterados para arrow function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,54 +2,55 @@ import User from '../models/userModel.js';
 
 import nodemailer from 'nodemailer';
 
-let user = {
-    async register(req, res) {
-        let checkUser = await User.findOne({ email: req.body.email });
+const register = async (req, res) => {
+    let checkUser = await User.findOne({ email: req.body.email });
 
-        if (checkUser) {
-            return res.status(400).send('Usuario ja cadastrado');
-        }
+    if (checkUser) {
+        return res.status(400).send('Usuario ja cadastrado');
+    }
 
-        const newUser = new User({
-            nome: req.body.nome,
-            email: req.body.email
-        });
+    const newUser = new User({
+        nome: req.body.nome,
+        email: req.body.email
+    });
 
-        try {
-            const saveUser = await newUser.save();
+    try {
+        const saveUser = await newUser.save();
 
-            res.send(saveUser);
-        } catch (error) {
-            res.status(400).send(error);
-        }
-    },
+        if (!saveUser) return res.status(400).send('Falha no cadastramento!');
 
-    async sendEmail(req, res) {
-        const transport = nodemailer.createTransport({
-            host: process.env.EMAIL_HOST,
-            port: process.env.EMAIL_PORT,
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASSWORD
-            }
-        });
-
-        try {
-            transport
-                .sendMail({
-                    from: process.env.EMAIL_USER,
-                    to: req.body.email,
-                    replyTo: process.env.EMAIL_USER,
-                    subject: 'Cadastro realizado com sucesso!',
-                    html: '<h1>Parabens!!! seu cadastro foi concluido com sucesso</h1>'
-                })
-                .then(response => {
-                    res.send(response);
-                });
-        } catch (error) {
-            res.status(400).send(error);
-        }
+        res.status(200).send(saveUser);
+    } catch (error) {
+        throw error;
     }
 };
 
-export default user;
+const sendEmail = async (req, res) => {
+    const transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    });
+
+    try {
+        const emailResponse = await transport.sendMail({
+            from: process.env.EMAIL_USER,
+            to: req.body.email,
+            replyTo: process.env.EMAIL_USER,
+            subject: 'Cadastro realizado com sucesso!',
+            html: '<h1>Parabens!!! seu cadastro foi concluido com sucesso</h1>'
+        });
+
+        if (!emailResponse)
+            return res.status(400).send('Falha ao enviar o email!');
+
+        res.status(200).send('Email enviado com sucesso!');
+    } catch (error) {
+        throw error;
+    }
+};
+
+export { register, sendEmail };
